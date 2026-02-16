e012df3def6b4c6978ff3c93c9b20a7c68c7d3b5
m06903630-rgb <m06903630@gmail.com>
2026-02-16 09:59:01 +0530

    Create sum.java

---
+++ sum.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+class program1
+  {
+    public static void main (String args[])
+    {
+      int a;
+      int b;
+      Scanner S=new Scanner(system.in);
+      system.out.println("Enter the value for a:");
+      a=S.nextInt();
+      system.out.println("Enter the value for b:");
+      b=S.nextInt();
+      int sum=a+b;
+      system.out.println("Sum=:",sum);
+    }
+  }
